3cccdd2dc24db5a5dd85c1754a3a484fbfd3f357
Evgeny <emonin@smuit.ru>
2019-10-07 17:31:04 +0400

    Events from UnityEvent<T>
    + void, bool, int, float, string, Vector2, Vector3, Quaternion, object:
    + example scene;

---
+++ Assets/cucutools/cucuevents/Scripts/CucuEvent.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace cucu.tools
+{
+    /// <inheritdoc />
+    public class CucuEvent : UnityEvent
+    {
+    }
+
+    /// <inheritdoc />
+    public class CucuBoolEvent : UnityEvent<bool>
+    {
+    }
+
+    /// <inheritdoc />
+    public class CucuIntEvent : UnityEvent<int>
+    {
+    }
+
+    /// <inheritdoc />
+    public class CucuFloatEvent : UnityEvent<float>
+    {
+    }
+
+    /// <inheritdoc />
+    public class CucuStringEvent : UnityEvent<string>
+    {
+    }
+
+    /// <inheritdoc />
+    public class CucuVector2Event : UnityEvent<Vector2>
+    {
+    }
+
+    /// <inheritdoc />
+    public class CucuVector3Event : UnityEvent<Vector3>
+    {
+    }
+
+    /// <inheritdoc />
+    public class CucuQuaternionEvent : UnityEvent<Quaternion>
+    {
+    }
+
+    /// <inheritdoc />
+    public class CucuObjectEvent : UnityEvent<object>
+    {
+    }
+}
