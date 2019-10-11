9fee30120cd048a4b3dc136deb70d1b02f076418
Evgeny <emonin@smuit.ru>
2019-10-11 17:51:52 +0400

    First look cucutrigger

--- Assets/cucutools/cucutrigger/Scripts/CucuTrigger.cs
+++ Assets/cucutools/cucutrigger/Scripts/CucuTrigger.cs
@@ -4,5 +4,23 @@ namespace cucu.tools
 {
     public class CucuTrigger : MonoBehaviour
     {
+        private Collider _collider;
+        private Rigidbody _rigidbody;
+
+        private void Awake()
+        {
+            _collider = GetComponent<Collider>();
+            _collider.isTrigger = true;
+
+            _rigidbody = GetComponent<Rigidbody>();
+            if (_rigidbody == null) _rigidbody = gameObject.AddComponent<Rigidbody>();
+            _rigidbody.useGravity = false;
+            _rigidbody.isKinematic = true;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            Debug.Log(other.gameObject.name);
+        }
     }
 }
