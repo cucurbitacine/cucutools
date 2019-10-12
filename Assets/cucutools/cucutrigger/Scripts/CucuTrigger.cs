19f77daebfa55ab1bd08061c2d365063ab0dbc71
Evgeny <monin23.23@yandex.ru>
2019-10-12 18:31:16 +0400

    Register types

--- Assets/cucutools/cucutrigger/Scripts/CucuTrigger.cs
+++ Assets/cucutools/cucutrigger/Scripts/CucuTrigger.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 
 namespace cucu.tools
 {
@@ -6,6 +9,13 @@ namespace cucu.tools
     {
         private Collider _collider;
         private Rigidbody _rigidbody;
+
+        [SerializeField] private Component[] _registerComponents;
+
+        private Dictionary<Type, CucuObjectEvent> _registeredTypes;
+
+        public IReadOnlyList<Type> RegisteredTypesList => _registeredTypes.Keys.ToList();
+        [SerializeField] private string[] _typeNames;
 
         private void Awake()
         {
@@ -16,11 +26,52 @@ namespace cucu.tools
             if (_rigidbody == null) _rigidbody = gameObject.AddComponent<Rigidbody>();
             _rigidbody.useGravity = false;
             _rigidbody.isKinematic = true;
+
+            _registeredTypes = new Dictionary<Type, CucuObjectEvent>();
+            foreach (var comp in _registerComponents) RegisterComponent(comp.GetType());
+        }
+
+        public CucuObjectEvent RegisterComponent<TComponent>()
+            where TComponent : Component =>
+            RegisterComponent(typeof(TComponent));
+
+        public void RemoveComponent<TComponent>()
+            where TComponent : Component =>
+            RemoveComponent(typeof(TComponent));
+
+        public CucuObjectEvent RegisterComponent(Type type)
+        {
+            if (_registeredTypes == null) _registeredTypes = new Dictionary<Type, CucuObjectEvent>();
+            if (!_registeredTypes.ContainsKey(type))
+            {
+                _registeredTypes.Add(type, new CucuObjectEvent());
+                UpdateViewList();
+            }
+            return _registeredTypes[type];
+        }
+        
+        public void RemoveComponent(Type type)
+        {
+            if (_registeredTypes.TryGetValue(type, out var cucuEvent))
+            {
+                cucuEvent.RemoveAllListeners();
+                if (_registeredTypes.Remove(type))
+                    UpdateViewList();
+            }
+        }
+
+        private void UpdateViewList()
+        {
+            _typeNames = RegisteredTypesList
+                .Select(rtl => rtl.ToString())
+                .ToArray();
         }
 
         private void OnTriggerEnter(Collider other)
         {
-            Debug.Log(other.gameObject.name);
+            foreach(var component in other.gameObject.GetComponents<Component>())
+                if (_registeredTypes.TryGetValue(component.GetType(), out var cucuEvent))
+                    cucuEvent.Invoke(component);                    
         }
     }
 }
---
+++ Assets/examples/cucutrigger/Scripts/ExampleCucuTrigger.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using cucu.tools;
+
+namespace cucu.example
+{
+    public class ExampleCucuTrigger : MonoBehaviour
+    {
+        [SerializeField] private CucuTrigger _trigger;
+        private void Start()
+        {
+            var types = _trigger.RegisteredTypesList;
+            foreach(var type in types)
+            {
+                _trigger.RegisterComponent(type).AddListener(obj=>
+                {
+                    var gameObject = (obj as Component).gameObject;
+
+                    Debug.Log($"[From editor] : [{gameObject.name}] : [{obj.GetType()}]");
+                });
+            }
+
+            _trigger.RegisterComponent<Transform>().AddListener(obj=>
+            {
+                var transform = obj as Transform;
+                transform.rotation = Random.rotation;
+
+                Debug.Log($"[From code] : [{transform.gameObject.name}] : [{obj.GetType()}]");
+            });
+        }
+    }
+}
+
+
