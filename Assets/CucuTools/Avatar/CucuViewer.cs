81175ad990196252e2705502baddda03b46ed87e
Evgeny <monin23.23@yandex.ru>
2020-07-22 13:56:21 +0400

    Walk and view

---
+++ Assets/CucuTools/Avatar/CucuViewer.cs
@@ -0,0 +1,181 @@
+﻿using UnityEngine;
+
+namespace CucuTools
+{
+    public class CucuViewer : MonoBehaviour
+    {
+        private const float SENSITIVITY_MIN = 0.001f;
+        private const float SENSITIVITY_MAX = 10f;
+
+        public float SensitivityHorizontal => sensitivityHorizontal;
+        public float SensitivityVertical => sensitivityVertical;
+
+        #region Serialized fields
+
+        [Header("Settings")] [SerializeField]
+        private bool isActive = true;
+
+        [SerializeField] [Range(SENSITIVITY_MIN, SENSITIVITY_MAX)]
+        private float sensitivityHorizontal = 2;
+
+        [SerializeField] [Range(SENSITIVITY_MIN, SENSITIVITY_MAX)]
+        private float sensitivityVertical = 2;
+
+        [SerializeField] [Range(0f, 90f)] private float yFieldViewMax = 90f;
+        [SerializeField] [Range(-90f, 0f)] private float yFieldViewMin = -90f;
+
+        [Header("References")] [SerializeField] [Tooltip("If is null, get self transform")]
+        private Transform horizontalTransform;
+
+        [SerializeField] [Tooltip("If is null, try get child camera or self transform")]
+        private Transform verticalTransform;
+
+        #endregion
+        
+        [Header("Info")]
+        [SerializeField] private Vector2 view;
+        [SerializeField] private float mouseXInput;
+        [SerializeField] private float mouseYInput;
+        
+        private void Awake()
+        {
+            if (horizontalTransform == null)
+                SetHorizontalTransform(transform);
+            if (verticalTransform == null)
+                SetVerticalTransform(GetComponentInChildren<Camera>()?.transform ?? transform);
+            
+#if UNITY_EDITOR
+            Active = false;
+            Cursor.visible = true;
+#endif
+            
+            SetupStartRotation();
+        }
+
+        private void Update()
+        {
+            UpdateInput();
+            
+            UpdateViewer(Time.deltaTime);
+        }
+
+        public void SetSensitivity(float axisX, float axisY)
+        {
+            SetSensitivityAxisX(axisX);
+            SetSensitivityAxisY(axisY);
+        }
+
+        public void SetSensitivityAxisX(float axisX)
+        {
+            sensitivityHorizontal = Mathf.Clamp(axisX, SENSITIVITY_MIN, SENSITIVITY_MAX);
+        }
+
+        public void SetSensitivityAxisY(float axisY)
+        {
+            sensitivityVertical = Mathf.Clamp(axisY, SENSITIVITY_MIN, SENSITIVITY_MAX);
+        }
+
+        public void SetActive(bool value)
+        {
+            isActive = value;
+        }
+
+        private void SetupStartRotation()
+        {
+            view = new Vector2(horizontalTransform.localRotation.eulerAngles.y, verticalTransform.localRotation.eulerAngles.x);
+        }
+        
+        public void Look(Vector2 look)
+        {
+            if (!Active) return;
+            
+            view += new Vector2(look.x * SensitivityHorizontal, look.y * SensitivityVertical);
+            
+            ValidationView(ref view);
+            
+            Look(view.x, view.y);
+        }
+
+        private void Look(float angleHorizontal, float angleVertical)
+        {
+            ValidationAngles(ref angleHorizontal, ref angleVertical);
+
+            var eulerLeftRight = horizontalTransform.localRotation.eulerAngles;
+            var eulerDownUp = verticalTransform.localRotation.eulerAngles;
+
+            horizontalTransform.localRotation = Quaternion.Euler(eulerLeftRight.x, angleHorizontal, eulerLeftRight.z);
+            verticalTransform.localRotation = Quaternion.Euler(-angleVertical, eulerDownUp.y, eulerDownUp.z);
+        }
+
+        private void ValidationView(ref Vector2 view)
+        {
+            var x = view.x;
+            var y = view.y;
+
+            ValidationAngles(ref x, ref y);
+
+            view.x = x;
+            view.y = y;
+        }
+
+        private void ValidationAngles(ref float horizontal, ref float vertical)
+        {
+            if (horizontal > 360) horizontal -= 360;
+            if (horizontal < 0) horizontal += 360;
+
+            if (vertical > yFieldViewMax) vertical = yFieldViewMax;
+            if (vertical < yFieldViewMin) vertical = yFieldViewMin;
+        }
+
+        public bool Active
+        {
+            get => isActive;
+            set => SetActive(value);
+        }
+
+        public Vector2 SensitivityView
+        {
+            get => new Vector2(sensitivityHorizontal, sensitivityVertical);
+            set
+            {
+                sensitivityHorizontal = value.x;
+                sensitivityVertical = value.y;
+            }
+        }
+
+        public void SetHorizontalTransform(Transform transform)
+        {
+            horizontalTransform = transform;
+        }
+        
+        public void SetVerticalTransform(Transform transform)
+        {
+            verticalTransform = transform;
+        }
+
+        private void UpdateViewer(float deltaTime)
+        {
+            if (!Active) return;
+            
+            UpdateRotation(deltaTime);
+        }
+
+        private void UpdateInput()
+        {
+            mouseXInput = Input.GetAxis("Mouse X");
+            mouseYInput = Input.GetAxis("Mouse Y");
+            
+#if UNITY_EDITOR
+            Active = Input.GetButton("Fire2");
+            Cursor.visible = !Active;
+#endif
+        }
+
+        private void UpdateRotation(float deltaTime)
+        {
+            var look = new Vector2(mouseXInput, mouseYInput) * deltaTime * 100f;
+
+            Look(look);
+        }
+    }
+}
---
+++ Assets/CucuTools/Avatar/CucuWalker.cs
@@ -0,0 +1,154 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace CucuTools
+{
+    [RequireComponent(typeof(CharacterController))]
+    public class CucuWalker : MonoBehaviour
+    {
+        #region Public
+
+        public bool Active
+        {
+            get => active;
+            set => active = value;
+        }
+
+        public bool CanWalk
+        {
+            get => canWalk;
+            set => canWalk = value;
+        }
+
+        public bool CanJump
+        {
+            get => canJump;
+            set => canJump = value;
+        }
+        
+        public bool UseGravity
+        {
+            get => useGravity;
+            set => useGravity = value;
+        }
+        
+        public float Speed
+        {
+            get => speed;
+            set => speed = value;
+        }
+
+        public UnityEvent<float> OnJumpEvent { get; } = new CucuFloatEvent();
+
+        #endregion
+
+        #region Private
+
+        private float Gravity => Physics.gravity.y;
+
+        [Header("Settings")]
+        [SerializeField] private bool active = true;
+        [SerializeField] private bool canWalk = true;
+        [SerializeField] private bool canJump = true;
+        [SerializeField] private bool useGravity = true;
+        [SerializeField] [Range(0f, 20f)] private float speed = 3f;
+        [SerializeField] [Range(0f, 03f)] private float jumpHeight = 0.5f;
+
+        [Header("References")]
+        [SerializeField] private CharacterController character;
+
+        [Header("Info")]
+        [SerializeField] private Vector2 inputInfo;
+        [SerializeField] private float velocityYInfo;
+        [SerializeField] private bool isGroundedInfo;
+
+        private float horizontalInput;
+        private float verticalInput;
+        private float velocityY;
+        private bool jumpInput;
+
+        #endregion
+
+        #region MonoBehaviour
+
+        private void Awake()
+        {
+            character = GetComponent<CharacterController>();
+        }
+
+        private void Update()
+        {
+            UpdateInput();
+
+            UpdateMovements(Time.deltaTime);
+
+#if UNITY_EDITOR
+            UpdateInfo();
+#endif
+        }
+
+        #endregion
+
+        #region Update methods
+
+        private void UpdateMovements(float deltaTime)
+        {
+            if (!Active) return;
+
+            var move = Vector3.zero;
+            move += GetXZMovement(deltaTime);
+            move += GetYMovement(deltaTime);
+
+            character.Move(move * deltaTime);
+        }
+
+        private void UpdateInput()
+        {
+            horizontalInput = Input.GetAxis("Horizontal");
+            verticalInput = Input.GetAxis("Vertical");
+            jumpInput = Input.GetButtonDown("Jump");
+        }
+
+        private void UpdateInfo()
+        {
+            inputInfo = new Vector2(horizontalInput, verticalInput);
+            velocityYInfo = velocityY;
+            isGroundedInfo = character.isGrounded;
+        }
+
+        #endregion
+
+        #region Caclulate movements
+
+        private Vector3 GetXZMovement(float deltaTime)
+        {
+            return CanWalk
+                ? speed * (horizontalInput * character.transform.right + verticalInput * character.transform.forward)
+                : Vector3.zero;
+        }
+
+        private Vector3 GetYMovement(float deltaTime)
+        {
+            if (character.isGrounded)
+            {
+                if (UseGravity && CanJump && jumpInput)
+                {
+                    velocityY = Mathf.Sqrt(jumpHeight * 2f * -Gravity);
+                    OnJumpEvent.Invoke(jumpHeight);
+                }
+                //else if(velocityY < 0f) velocityY = 0.0f; TODO doesn't work...
+            }
+            else
+            {
+                if (UseGravity)
+                    velocityY += Gravity * deltaTime;
+                else
+                    velocityY = 0.0f;
+            }
+
+            return Vector3.up * velocityY;
+        }
+
+        #endregion
+    }
+}
