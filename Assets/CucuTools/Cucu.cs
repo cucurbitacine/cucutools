db2eec45ccf34bbb46b75ecef446830929b40d63
evgeny <monin23.23@yandex.ru>
2021-05-31 09:53:14 +0400

    Colors

--- Assets/CucuTools/Cucu.cs
+++ Assets/CucuTools/Cucu.cs
@@ -1,3 +1,5 @@
+using UnityEngine;
+
 namespace CucuTools
 {
     public static class Cucu
@@ -9,5 +11,40 @@ namespace CucuTools
         public const string Tools = "Tools/" + CreateAsset;
         
         public const string WorkflowGroup = "Workflow/";
+        
+        public static float[] LinSpace(float origin, float target, int count)
+        {
+            var isForward = count >= 0;
+
+            count = Mathf.Abs(count);
+
+            var result = new float[count];
+
+            switch (count)
+            {
+                case 0:
+                    return result;
+                case 1:
+                    result[0] = isForward ? origin : target;
+                    return result;
+                case 2:
+                    result[0] = isForward ? origin : target;
+                    result[1] = isForward ? target : origin;
+                    return result;
+                default:
+                {
+                    result[0] = isForward ? origin : target;
+                    result[count - 1] = isForward ? target : origin;
+                    var step = (isForward ? 1 : -1) * (target - origin) / (count - 1);
+                    for (var i = 1; i < count - 1; i++) result[i] = result[0] + step * i;
+                    return result;
+                }
+            }
+        }
+
+        public static float[] LinSpace(int count)
+        {
+            return LinSpace(0f, 1f, count);
+        }
     }
 }
