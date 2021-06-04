e072583c8c73322c34218ed273635ea9ea70ab57
Evgeny <emonin@smuit.ru>
2021-06-04 13:55:18 +0400

    Workflow Viewer

--- Assets/CucuTools/Workflows/Core/SwitchEntity.cs
+++ Assets/CucuTools/Workflows/Core/SwitchEntity.cs
@@ -1,13 +1,21 @@
 using System;
 using System.Linq;
-using CucuTools.Buttons;
 using UnityEngine;
 
 namespace CucuTools.Workflows.Core
 {
-    public abstract class SwitchEntity<T> : TransitionEntity, IStateTrigger
+    public abstract class SwitchEntity : TransitionEntity, IStateTrigger
+    {
+        public abstract void Invoke(StateInvoke mode);
+        
+        public abstract StateEntity[] Targets { get; }
+    }
+    
+    public abstract class SwitchEntity<T> : SwitchEntity
     {
         public const string AddSwitch = Cucu.AddComponent + Cucu.WorkflowGroup + "Switch/";
+
+        public override StateEntity[] Targets => caseValues.Select(c => c.target).ToArray();
         
         public override ConditionEntity[] Conditions { get; set; }
         public override bool IsReady => CheckCase();
@@ -34,12 +42,12 @@ namespace CucuTools.Workflows.Core
         
         [Header("Info")]
         [SerializeField] protected T value;
-        [SerializeField] protected  StateEntity target;
+        [SerializeField] protected StateEntity target;
         
         [Header("Cases")]
         [SerializeField] protected CaseValue<T>[] caseValues;
         
-        public void Invoke(StateInvoke mode)
+        public override void Invoke(StateInvoke mode)
         {
             if (Owner == null) return;
             if (mode != StateInvoke.OnStart) return;
